4929044a005d79e3223c121c7b3bb6ca8e6498ff
Joao Ciardullo <jciardullo.deal@veloe.com.br>
2024-02-19 21:40:46 -0300

    adicionando outros endpooint

--- src/main/java/br/com/joaozinho/dynamodbcrud/controller/PlayerController.java
+++ src/main/java/br/com/joaozinho/dynamodbcrud/controller/PlayerController.java
@@ -2,26 +2,96 @@ package br.com.joaozinho.dynamodbcrud.controller;
 
 import br.com.joaozinho.dynamodbcrud.entity.PlayerHistory;
 import br.com.joaozinho.dynamodbcrud.records.ScoreDTO;
-import br.com.joaozinho.dynamodbcrud.service.PlayerService;
-import org.springframework.http.HttpStatus;
+import io.awspring.cloud.dynamodb.DynamoDbTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import software.amazon.awssdk.enhanced.dynamodb.Key;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryConditional;
+import software.amazon.awssdk.enhanced.dynamodb.model.QueryEnhancedRequest;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/players")
 public class PlayerController {
-    private final PlayerService service;
+    private final DynamoDbTemplate dynamoDbTemplate;
 
-    public PlayerController(PlayerService service) {
-        this.service = service;
+    public PlayerController(DynamoDbTemplate dynamoDbTemplate) {
+        this.dynamoDbTemplate = dynamoDbTemplate;
     }
 
-
     @PostMapping("/{playerId}/games")
-    public ResponseEntity<PlayerHistory> criandoPlayer(@PathVariable("playerId") String playerId,
-                                                       @RequestBody ScoreDTO scoreDto) {
-        PlayerHistory playerHistory = service.create(playerId, scoreDto);
+    public void create(@PathVariable("playerId") String playerId,
+                       @RequestBody ScoreDTO scoreDto) {
+        var playerHistory = PlayerHistory.fromScore(playerId, scoreDto);
+        dynamoDbTemplate.save(playerHistory);
+    }
 
-        return new ResponseEntity<>(playerHistory, HttpStatus.CREATED);
+    @GetMapping("/{playerId}/games")
+    public ResponseEntity<List<PlayerHistory>> listGames(@PathVariable("playerId") String playerId) {
+
+        var key = Key.builder().partitionValue(playerId).build();
+
+        var conditional = QueryConditional.keyEqualTo(key);
+
+        var playerHistoryList = dynamoDbTemplate.query(QueryEnhancedRequest.builder()
+                        .queryConditional(conditional).build(),
+                PlayerHistory.class);
+
+        return ResponseEntity.ok(playerHistoryList.items().stream().toList());
+
+    }
+
+    @GetMapping("/{playerId}/games/{gameId}")
+    public ResponseEntity<PlayerHistory> getById(@PathVariable("playerId") String playerId,
+                                                 @PathVariable("gameId") String gameId) {
+        var user = dynamoDbTemplate.load(Key.builder()
+                .partitionValue(playerId)
+                .sortValue(gameId)
+                .build(), PlayerHistory.class);
+
+        return user == null ?
+                ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+    }
+
+    @DeleteMapping("/{playerId}/games/{gameId}")
+    public ResponseEntity<Void> delete(@PathVariable("playerId") String playerId,
+                                       @PathVariable("gameId") String gameId) {
+        var key = Key.builder()
+                .partitionValue(playerId)
+                .sortValue(gameId)
+                .build();
+
+        var player = dynamoDbTemplate.load(key, PlayerHistory.class);
+
+        if (player == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        dynamoDbTemplate.delete(key, PlayerHistory.class);
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{playerId}/games/{gameId}")
+    public ResponseEntity<Void> update(@PathVariable("playerId") String playerId,
+                                       @PathVariable("gameId") String gameId,
+                                       @RequestBody ScoreDTO scoreDto) {
+        var key = Key.builder()
+                .partitionValue(playerId)
+                .sortValue(gameId)
+                .build();
+
+        var player = dynamoDbTemplate.load(key, PlayerHistory.class);
+
+        if (player == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        player.setScore(scoreDto.score());
+
+        dynamoDbTemplate.save(player);
+
+        return ResponseEntity.noContent().build();
     }
 }
